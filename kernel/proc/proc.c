2585e0ed46b155870f172e56712e72c718e9cb5a
derek-yi <derek_yi@163.com>
2020-03-25 17:13:06 +0800

    sysfs

--- kernel/proc/proc.c
+++ kernel/proc/proc.c
@@ -7,18 +7,70 @@
 #include <linux/proc_fs.h>  
 #include <linux/fs.h>  
 #include <linux/seq_file.h>   
-
+#include <linux/platform_device.h>
+#include <asm/io.h>
+#include <linux/slab.h> /* kmalloc, kfree */
+#include <linux/irq.h> /* IRQ_TYPE_EDGE_BOTH */
+#include <linux/interrupt.h>
 #include <asm/uaccess.h>  
+
  
-#define STRING_LEN 1024  
+
+#if 1
+
+int intr_cnt = 0;
+
+void my_tasklet_func(unsigned long data)
+{
+    printk("my_tasklet_func called\n");
+    intr_cnt++;
+}
+
+DECLARE_TASKLET(xx_tasklet, my_tasklet_func, 0);
+
+struct work_struct xx_wq;
+
+void my_wq_func(struct work_struct *work)
+{
+    printk("my_wq_func called\n");
+    intr_cnt += 100;
+}
+
+static int user_cmd_proc(char *user_cmd)
+{
+    printk("user_cmd_proc: %s", user_cmd);
+
+    if(strncmp(user_cmd, "tasklet", 7) == 0) {
+        tasklet_schedule(&xx_tasklet);
+    }
+
+    if(strncmp(user_cmd, "workqueue", 9) == 0) {
+        schedule_work(&xx_wq);
+    }
+    
+    return 0;
+}
+
+
+
+
+#endif
+
+
+#if 1
+
+#define MAX_CMD_LEN 256  
  
-char global_buffer[STRING_LEN] = {0};
+char global_buffer[MAX_CMD_LEN] = {0};
 
 static int my_proc_show(struct seq_file *seq, void *v)
 {
     printk("my_proc_show called\n");
+    
     seq_printf(seq, "current kernel time is %ld\n", jiffies);  
-    seq_printf(seq, "global_buffer: %s", global_buffer);
+    seq_printf(seq, "cmd: tasklet, workqueue \n");
+    
+    seq_printf(seq, "last user cmd: %s", global_buffer);
 
     return 0;        
 }
@@ -30,10 +82,14 @@ static int my_proc_open(struct inode *inode, struct file *file)
 
 static ssize_t my_proc_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
 {
-    if (count > 0) {
-        printk("my_proc_write called\n");
-        copy_from_user(global_buffer, buffer, count);  
+    printk("my_proc_write called\n");
+    if (count < 1 || count >= MAX_CMD_LEN ) {
+        printk("invalid user cmd\n");
+        return 0;
     }
+
+    copy_from_user(global_buffer, buffer, count);  
+    user_cmd_proc(global_buffer);
 
     return count;
 } 
@@ -64,7 +120,9 @@ static int __init proc_test_init(void)
          return 0;
     }
 
-    strcpy(global_buffer, "hello"); 
+    //init workqueue
+    INIT_WORK(&xx_wq, my_wq_func);
+    
     return 0;  
 }  
  
@@ -73,7 +131,9 @@ static void __exit proc_test_exit(void)
     remove_proc_entry("buffer", proc_dir);  
     remove_proc_entry("my_proc", NULL);  
 }  
- 
+
+#endif
+
 module_init(proc_test_init);  
 module_exit(proc_test_exit);
 MODULE_AUTHOR("derek.yi");  
---
+++ kernel/sysfs/sysfs.c
@@ -0,0 +1,105 @@
+
+#include <linux/init.h>             
+#include <linux/module.h>           
+#include <linux/kernel.h>
+#include <linux/kthread.h>      
+#include <linux/delay.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <linux/gpio.h>
+
+
+static int led_status = 0;
+
+#define LED_PIN   26
+
+static struct kobject *my_kobj;
+
+#if 1
+static ssize_t led_show(struct kobject* kobjs, struct kobj_attribute *attr, char *buf)
+{
+    printk(KERN_INFO "led_show\n");
+    return sprintf(buf, "The led status = %d\n", led_status);
+}
+#endif
+
+static ssize_t led_status_show(struct kobject* kobjs, struct kobj_attribute *attr, char *buf)
+{
+    printk(KERN_INFO "led_status_show\n");
+    return sprintf(buf, "led status: %d\n", led_status);
+}
+
+static ssize_t led_status_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+    printk(KERN_INFO "led_status_store\n");
+    
+    if(0 == memcmp(buf, "on", 2))
+    {
+        //gpio_set_value(LED_PIN,1);
+        led_status = 1;
+    }
+    else if(0 == memcmp(buf, "off", 3))
+    {
+        //gpio_set_value(LED_PIN, 0);
+        led_status = 0;
+    }
+    else
+    {
+        printk(KERN_INFO "Not support cmd\n");
+    }
+    
+    return count;
+}
+
+static struct kobj_attribute status_attr = __ATTR_RO(led);
+static struct kobj_attribute led_attr = __ATTR(led_status, 0660, led_status_show, led_status_store);  //Doesn't support 0666 in new version.
+
+
+static struct attribute *led_attrs[] = {
+    &status_attr.attr,
+    &led_attr.attr,
+    NULL,
+};
+
+static struct attribute_group led_attr_grp = {
+    .name = "led_test",
+    .attrs = led_attrs,
+};
+
+
+static int __init sysfs_ctrl_init(void)
+{
+    printk(KERN_INFO "sysfs_ctrl_init\n");
+    
+    my_kobj = kobject_create_and_add("obj_test", kernel_kobj->parent);
+    
+    sysfs_create_group(my_kobj, &led_attr_grp);
+    
+    return 0;
+}
+
+static void __exit sysfs_ctrl_exit(void)
+{
+    sysfs_remove_group(my_kobj, &led_attr_grp);
+    
+    kobject_put(my_kobj);
+    
+    printk(KERN_INFO "sysfs_ctrl_exit\n");
+}
+
+/*
+cat /sys/obj_test/led_test/led
+cat /sys/obj_test/led_test/led_status
+chmod 666 /sys/obj_test/led_test/led_status
+echo "off" > /sys/obj_test/led_test/led_status
+echo "on" > /sys/obj_test/led_test/led_status
+*/
+
+module_init(sysfs_ctrl_init);
+module_exit(sysfs_ctrl_exit);
+
+MODULE_AUTHOR("derek.yi");  
+MODULE_LICENSE("GPL");  
+
