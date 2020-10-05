9d2832041ed1c56324c9c0006e101421c900f536
derel-yi <derek_yi@163.com>
2020-10-05 01:33:39 -0700

    new

--- socket/uds_rpc.c
+++ socket/uds_rpc.c
@@ -16,104 +16,9 @@
 #define BUFFER_SIZE         512
 
 
-#if xxx
 
-char* server_file = "server.sock";
-char* client_file = "client.sock";
-
-
-int create_client(void)
-{
-    struct sockaddr_un srv_addr;
-    struct sockaddr_un cli_addr;
-    int sock_fd;
-    char buffer[BUFFER_SIZE];
-
-    sock_fd = socket(AF_UNIX, SOCK_DGRAM,0);
-    if(sock_fd < 0){
-        perror("Request socket failed\n");
-        return -1;
-    }
-
-    cli_addr.sun_family = AF_UNIX;
-    unlink(client_file);
-    strcpy(cli_addr.sun_path,client_file);
-    if(bind(sock_fd, &cli_addr, sizeof(cli_addr)) <0 ){
-        perror("bind failed!\n");
-        return -1;
-    }
-
-    srv_addr.sun_family = AF_UNIX;
-    strcpy(srv_addr.sun_path, server_file);
-    socklen_t len = sizeof(srv_addr);
-    while(1) {
-        char *p = "Hello, uds server";
-        int ssize = sendto(sock_fd, p, strlen(p), 0, &srv_addr, len);
-        if (ssize < 0) {
-            perror("sendto");
-            break;
-        }
-
-        int size = recvfrom(sock_fd, buffer,sizeof(buffer),0, &srv_addr, &len);
-        if (size < 0) {
-            perror("recv");
-            break;
-        } else {
-            printf("client recv: %s\n", buffer);
-        }
-        sleep(3);
-    }
-
-    close(sock_fd);
-    return 0;
-}
-
-
-int create_server()
-{
-    int sock_fd;
-    struct sockaddr_un srv_addr;
-    
-    sock_fd = socket(AF_UNIX, SOCK_DGRAM, 0);
-    if(sock_fd < 0){
-        perror("Request socket failed!\n");
-        return -1;
-    }
-    
-    srv_addr.sun_family = AF_UNIX;
-    unlink(server_file);
-    strcpy(srv_addr.sun_path, server_file);
-    if(bind(sock_fd, &srv_addr, sizeof(srv_addr)) <0 ){
-        perror("bind failed!\n");
-        return -1;
-    }
-    
-    while(1){
-        struct sockaddr_un cli_addr;
-        socklen_t len = sizeof(cli_addr);
-        char buffer[BUFFER_SIZE];
-        
-        bzero(buffer, BUFFER_SIZE);
-        int ret = recvfrom(sock_fd, buffer, sizeof(buffer), 0, &cli_addr, &len);
-        if(ret < 0){
-            printf("recv failed\n");
-        } else {
-            printf("server recv: %s\n", buffer);
-        }
-        
-        char *p = "OK,I got id!";
-        int ssize = sendto(sock_fd, p, strlen(p), 0, &cli_addr, len);
-        if(ssize < 0){
-            printf("send failed\n");
-        } 
-    }
-    
-    close(sock_fd);
-    return 0;
-}
-#endif
-
-int sock_fd = -1;
+int srv_fd = -1;
+int api_fd = -1;
 char my_uds_name[64];
 
 void uds_echo(void)  
@@ -122,21 +27,24 @@ void uds_echo(void)
     while(1){
         struct sockaddr_un peer_addr;
         socklen_t len = sizeof(peer_addr);
+        static int serial_num = 0;
         char buffer[BUFFER_SIZE];
         
         bzero(buffer, BUFFER_SIZE);
-        int ret = recvfrom(sock_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&peer_addr, &len);
+        int ret = recvfrom(srv_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&peer_addr, &len);
         if(ret < 0){
             printf("recv failed\n");
         } else {
-            printf("%s: recv: %s\n", my_uds_name, buffer);
+            printf("%s.srv: recv: %s\n", my_uds_name, buffer);
         }
         
-        char *p = "OK,I got id!";
-        int ssize = sendto(sock_fd, p, strlen(p), 0, (struct sockaddr *)&peer_addr, len);
+        sprintf(buffer, "echo sn %d", serial_num++);
+        int ssize = sendto(srv_fd, buffer, strlen(buffer), 0, (struct sockaddr *)&peer_addr, len);
         if(ssize < 0){
             printf("send failed\n");
-        } 
+        } else {
+            printf("%s.srv: send %s(%d)\n", my_uds_name, buffer, ssize);
+        }
     }
 }  
 
@@ -146,17 +54,32 @@ int create_uds_main(char *local_name, char *peer_name)
     struct sockaddr_un local_addr;
     struct sockaddr_un peer_addr;
     
-    sock_fd = socket(AF_UNIX, SOCK_DGRAM, 0);
-    if(sock_fd < 0){
+    api_fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if(api_fd < 0){
+        perror("Request socket failed!\n");
+        return -1;
+    }
+    
+    local_addr.sun_family = AF_UNIX;
+    strcpy(local_addr.sun_path, my_uds_name);
+    strcat(local_addr.sun_path, ".api");
+    unlink(local_addr.sun_path);
+    if (bind(api_fd, &local_addr, sizeof(local_addr)) <0 ) {
+        perror("bind failed!\n");
+        return -1;
+    }
+
+    srv_fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if (srv_fd < 0) {
         perror("Request socket failed!\n");
         return -1;
     }
 
-    sprintf(my_uds_name, "%s", local_name);
     local_addr.sun_family = AF_UNIX;
-    unlink(local_name);
-    strcpy(local_addr.sun_path, local_name);
-    if(bind(sock_fd, &local_addr, sizeof(local_addr)) <0 ){
+    strcpy(local_addr.sun_path, my_uds_name);
+    strcat(local_addr.sun_path, ".srv");
+    unlink(local_addr.sun_path);
+    if (bind(srv_fd, &local_addr, sizeof(local_addr)) <0 ) {
         perror("bind failed!\n");
         return -1;
     }
@@ -169,12 +92,14 @@ int create_uds_main(char *local_name, char *peer_name)
 
     peer_addr.sun_family = AF_UNIX;
     strcpy(peer_addr.sun_path, peer_name);
+    strcat(peer_addr.sun_path, ".srv");
     socklen_t len = sizeof(peer_addr);
     while(1) {
         char buffer[BUFFER_SIZE];
+        static int serial_num = 0;
 
-        sprintf(buffer, "hello, %s", peer_name);
-        int ssize = sendto(sock_fd, buffer, strlen(buffer), 0, (struct sockaddr *)&peer_addr, len);
+        sprintf(buffer, "hello %s, sn %d", peer_name, serial_num++);
+        int ssize = sendto(api_fd, buffer, strlen(buffer), 0, (struct sockaddr *)&peer_addr, len);
         if (ssize < 0) {
             perror("sendto");
             sleep(1);
@@ -182,19 +107,43 @@ int create_uds_main(char *local_name, char *peer_name)
         }
 
         memset(buffer, 0, sizeof(buffer));
-        int size = recvfrom(sock_fd, buffer,sizeof(buffer),0, (struct sockaddr *)&peer_addr, &len);
+        int size = recvfrom(api_fd, buffer,sizeof(buffer),0, (struct sockaddr *)&peer_addr, &len);
         if (size < 0) {
             perror("recv");
             break;
         } else {
-            printf("%s: recv: %s\n", local_name, buffer);
+            printf("%s.api: recv: %s\n", local_name, buffer);
         }
         sleep(3);
     }
-    
-    close(sock_fd);
+
+    close(srv_fd);
+    close(api_fd);
     return 0;
 }
+/*
+
+aa.srv
+bind aa.srv
+recv bb.api
+send bb.api
+
+aa.api
+bind aa.api
+send to bb.srv
+recv bb.srv
+
+bb.srv
+bind bb.src
+recv aa.api
+send aa.api
+
+bb.api
+bind bb.api
+send to aa.srv
+recv aa.srv
+
+*/
 
 //https://blog.csdn.net/briblue/article/details/89350869    
 int main(int argc, char **argv)
@@ -204,6 +153,7 @@ int main(int argc, char **argv)
         return 0;
     }
 
+    sprintf(my_uds_name, "%s", argv[1]);
     create_uds_main(argv[1], argv[2]);
     return 0;
 }
