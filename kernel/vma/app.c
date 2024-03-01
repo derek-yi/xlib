d28a74edfce2cc58b1cfa73cc8670989ff6303d1
Your Name <you@example.com>
2024-03-01 18:00:01 +0800

    vma

--- kernel/vma/app.c
+++ kernel/vma/app.c
@@ -1,49 +1,154 @@
-#include <stdio.h>
-#include <malloc.h>
- 
-static int global_data=1;
-static int global_data1;
-int bss_data;
-int bss_data1;
- 
+#include <stdio.h> 
+#include <fcntl.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+
+#define DMA_BUFF_SZ 1024
+
+static char global_data1[4096];
+static char global_data2[409600];
+char bss_data[4096];
+
+// translate a virtual address to a physical one via /proc/self/pagemap
+intptr_t virt_to_phys(void* virt) 
+{
+	long pagesize = sysconf(_SC_PAGESIZE);
+	int fd = open("/proc/self/pagemap", O_RDONLY);
+	intptr_t phy = 0;
+    int ret;
+    
+	// pagemap is an array of pointers for each normal-sized page
+	ret = lseek(fd, (intptr_t) virt / pagesize * sizeof(intptr_t), SEEK_SET);
+    if (ret < 0) return 0;
+
+	read(fd, &phy, sizeof(phy));
+	close(fd);
+    
+	if (!phy) {
+		//printf("failed to translate virtual address %p to physical address \n", virt);
+	}
+    
+	// bits 0-54 are the page number
+	return (phy & 0x7fffffffffffffULL) * pagesize + ((intptr_t) virt) % pagesize;
+}
+
 int main()
 {
+    int fd, ret;
     int stack_data = 1;
-    int stack_data1 = 2;
-    int data[200*1024];
- 
-    static int data_val=1;
- 
-    int* malloc_data=malloc(10);
-    int* malloc_data1=(int*)malloc(300);
-    int* malloc_data2=(int*)malloc(300*1024);
- 
-    // stack segment
-    printf("stack segment!\n");
-    printf("\t stack_data=0x%lx\n",&stack_data);
-    printf("\t stack_data1=0x%lx\n",&stack_data1);
- 
-    // heap segment
-    printf("heap segment!\n");
-    printf("\t malloc_data=0x%lx\n",malloc_data);
-    printf("\t malloc_data1=0x%lx\n",malloc_data1);
-    printf("\t malloc_data2=0x%lx\n",malloc_data2);
- 
+    int mem_buff[4096];
+    char *map_mem;
+    void *phy_addr;
+    static int static_data = 1;
+    char* malloc_data    = malloc(16);
+    char* malloc_data1   = malloc(512);
+    char* malloc_data2   = malloc(512*1024);
+
+    fd = open("/dev/vma_demo", O_RDWR);
+    if ( fd < 0 ) {
+        printf("open failed \n");
+        return 0;
+    }
+
+	map_mem = (char *)mmap(NULL, DMA_BUFF_SZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (map_mem != MAP_FAILED) {
+		//memset(map_mem, 0x10, DMA_BUFF_SZ);
+        printf("map_map 0x%x 0x%x 0x%x \n", map_mem[0], map_mem[1], map_mem[2]);
+        phy_addr = virt_to_phys(map_mem); //map failed
+		printf("map_map 0x%p, phy_addr 0x%lx \n", map_mem, phy_addr);
+	}
+
+	phy_addr = virt_to_phys(mem_buff);
+	printf("local va 0x%p, pa 0x%p \n", mem_buff, phy_addr);
+
     //code segment
-    printf("code segment!\n");
-    printf("\t code_data=0x%lx\n",main);
+    printf("pid %d \n", getpid());
+    printf("code segment:\n");
+    printf("\t code_data=0x%lx\n", (long)main);
  
     //data segment
-    printf("data segment!\n");
-    printf("\t global_data=0x%lx\n",&global_data);
-    printf("\t global_data1=0x%lx\n",&global_data1);
-    printf("\t data_val=0x%lx\n",&data_val);
+    printf("data segment:\n");
+    phy_addr = virt_to_phys(global_data1); //map failed
+    printf("\t global_data1=0x%lx 0x%lx\n", (long)global_data1, phy_addr);
+    printf("\t global_data2=0x%lx\n", (long)global_data2);
+    printf("\t static_data=0x%lx\n",  (long)&static_data);
  
     //bss segment
-    printf("bss segment!\n");
-    printf("\t bss_data=0x%lx\n",&bss_data);
-    printf("\t bss_data1=0x%lx\n",&bss_data1);
+    printf("bss segment:\n");
+    printf("\t bss_data=0x%lx\n", (long)bss_data);
+
+    // stack segment
+    printf("stack segment:\n");
+    printf("\t stack_data=0x%lx\n",     (long)&stack_data);
  
+    // heap segment
+    printf("heap segment:\n");
+    printf("\t malloc_data=0x%lx\n",  (long)malloc_data);
+    printf("\t malloc_data1=0x%lx 0x%lx\n", (long)malloc_data1, virt_to_phys(malloc_data1));
+    printf("\t malloc_data2=0x%lx 0x%lx\n", (long)malloc_data2, virt_to_phys(malloc_data2));
+
+	while(1) sleep(1);
+    if (map_mem != MAP_FAILED) munmap(map_mem, DMA_BUFF_SZ);
+    close(fd);
     return 0;
 }
+
+/*
+root@linaro-alip:/home/unisoc# ./a.out
+map_map 0x0 0x1 0x2
+map_map 0x0x7ff7ff7000, phy_addr 0x0
+local va 0x0x7fffffb798, pa 0x0x43ad798
+pid 87782
+code segment:
+         code_data=0x5555550afc
+data segment:
+         global_data1=0x5555563098 0x98
+         global_data2=0x5555564098
+         static_data=0x5555562088
+bss segment:
+         bss_data=0x5555562098
+stack segment:
+         stack_data=0x7ffffff79c
+heap segment:
+         malloc_data=0x55555c92a0
+         malloc_data1=0x55555c92c0 0x2db802c0
+
+cat /sys/devices/virtual/misc/vma_demo/cmd_buffer
+echo show > /sys/devices/virtual/misc/vma_demo/cmd_buffer
+
+root@linaro-alip:/home/unisoc# echo 87782 > /sys/devices/virtual/misc/vma_demo/cmd_buffer
+root@linaro-alip:/home/unisoc# dmesg
+[ 4054.249677] current pid 2455
+[ 4054.249698] mm_struct addr = 0x000000005657a49d
+[ 4054.249703]      1:         a67dc784   5555550000   5555552000     8192 flag 0x875
+[ 4054.249708]      2:         ddf495f7   5555561000   5555562000     4096 flag 0x100871
+[ 4054.249712]      3:         e1a9d5fe   5555562000   5555563000     4096 flag 0x100873
+[ 4054.249716]      4:         e072f598   5555563000   55555ea000   552960 flag 0x100073
+[ 4054.249720]      5:         8eb2fbde   7ff7dbd000   7ff7e3e000   528384 flag 0x100073
+[ 4054.249724]      6:         f6ec28b0   7ff7e3e000   7ff7f9a000  1425408 flag 0x75
+[ 4054.249728]      7:         a299a715   7ff7f9a000   7ff7fa9000    61440 flag 0x70
+[ 4054.249731]      8:         93892b81   7ff7fa9000   7ff7fad000    16384 flag 0x100071
+[ 4054.249735]      9:         febff3e3   7ff7fad000   7ff7faf000     8192 flag 0x100073
+[ 4054.249739]     10:         c01d770d   7ff7faf000   7ff7fb2000    12288 flag 0x100073
+[ 4054.249742]     11:         4dd35d49   7ff7fcc000   7ff7fed000   135168 flag 0x875
+[ 4054.249746]     12:         8eab9d62   7ff7ff7000   7ff7ff8000     4096 flag 0x40444fb
+[ 4054.249750]     13:         e6b833cb   7ff7ff8000   7ff7ffa000     8192 flag 0x100073
+[ 4054.249754]     14:         9ec1801a   7ff7ffa000   7ff7ffc000     8192 flag 0x40411
+[ 4054.249757]     15:         a718bef5   7ff7ffc000   7ff7ffd000     4096 flag 0x40075
+[ 4054.249761]     16:         94c541c1   7ff7ffd000   7ff7ffe000     4096 flag 0x100871
+[ 4054.249765]     17:         40b823a5   7ff7ffe000   7ff8000000     8192 flag 0x100873
+[ 4054.249768]     18:         3d07b433   7ffffdf000   8000000000   135168 flag 0x100173
+
+
+
+
+*/
 
--- kernel/vma/vma.c
+++ kernel/vma/vma.c
@@ -1,13 +1,29 @@
-//vma_test.c
+#include <linux/miscdevice.h>
+#include <linux/delay.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/mm.h>
-#include <linux/sched.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/delay.h>
+#include <linux/moduleparam.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+#include <linux/ioctl.h>
+#include <linux/cdev.h>
+#include <linux/string.h>
+#include <linux/list.h>
+#include <linux/pci.h>
+#include <linux/gpio.h>
+
+#define DEVICE_NAME "vma_demo"
+ 
+static unsigned char *share_mem;
 
 static int pid;
-module_param(pid, int, 0644);
 
-static void printit(struct task_struct *tsk)
+static void print_task_info(struct task_struct *tsk)
 {
   struct mm_struct *mm;
   struct vm_area_struct *vma;
@@ -19,64 +35,121 @@ static void printit(struct task_struct *tsk)
   vma = mm->mmap;
 
   /* 使用 mmap_sem 读写信号量进行保护 */
-  down_read(&mm->mmap_sem);
-  pr_info("vmas: vma start end length\n");
+  //down_read(&mm->mmap_sem);
 
   while (vma) {
       j++;
       start = vma->vm_start;
       end = vma->vm_end;
       length = end - start;
-      pr_info("%6d: %16p %12lx %12lx %8ld\n",
-          j, vma, start, end, length);
+      pr_info("%6d: %16p %12lx %12lx %8ld flag 0x%lx\n",
+          j, vma, start, end, length, vma->vm_flags);
       vma = vma->vm_next;
   }
-  up_read(&mm->mmap_sem);
+  //up_read(&mm->mmap_sem);
 }
 
-static int __init vma_init(void)
+static int my_open(struct inode *inode, struct file *file)
 {
-  struct task_struct *tsk;
-  /* 如果插入模块时未定义 pid 号，则使用当前 pid */
-  if (pid == 0) {
-      tsk = current;
-      pid = current->pid;
-      pr_info("using current process\n");
-  } else {
-      tsk = pid_task(find_vpid(pid), PIDTYPE_PID);
-  }
-  if (!tsk)
-      return -1;
-  pr_info(" Examining vma's for pid=%d, command=%s\n", pid, tsk->comm);
-  printit(tsk);
-  return 0;
+    return 0;
 }
-
-static void __exit vma_exit(void)
-{
-  pr_info("Module exit\n");
-}
-
-module_init(vma_init);
-module_exit(vma_exit);
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Mr Yu");
-MODULE_DESCRIPTION("vma test");
-
-
-/*
-root:/data # cat /proc/4766/maps
-00400000-0047c000 r-xp 00000000 103:23 6918                              /data/vma
-0048b000-0048e000 rw-p 0007b000 103:23 6918                              /data/vma
-0048e000-0048f000 rw-p 00000000 00:00 0
-38382000-383a4000 rw-p 00000000 00:00 0                                  [heap]
-78941af000-78941fb000 rw-p 00000000 00:00 0
-78941fb000-78941fc000 r--p 00000000 00:00 0                              [vvar]
-78941fc000-78941fd000 r-xp 00000000 00:00 0                              [vdso]
-7fc0ed3000-7fc0f9d000 rw-p 00000000 00:00 0                              [stack]
-*/
  
+static int my_map(struct file *filp, struct vm_area_struct *vma)
+{
+    vma->vm_flags |= VM_IO; //keep coherent
+    //vma->vm_flags |= VM_DONTEXPAND | VM_DONTDUMP; 
+    if (remap_pfn_range(vma,
+                       vma->vm_start,
+                       virt_to_phys(share_mem)>>PAGE_SHIFT,
+                       vma->vm_end - vma->vm_start,
+                       vma->vm_page_prot))
+    {  
+        return -EAGAIN;  
+    }
+
+    return 0;
+}
+ 
+static struct file_operations dev_fops = {
+    .owner = THIS_MODULE,
+    .open = my_open,
+    .mmap = my_map,
+};
+ 
+static struct miscdevice misc = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = DEVICE_NAME,
+    .fops = &dev_fops,
+};
+ 
+static ssize_t ext_sync_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "pid %d \n", pid);
+}
+
+static ssize_t ext_sync_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t len)
+{
+    struct task_struct *tsk;
+
+	if ( memcmp(buf, "show", 4) == 0 ) {
+        pid = current->pid;
+        pr_info("current pid %d\n", pid);
+        pr_info("share_mem 0x%lx, phy 0x%lx \n", (long)share_mem, (long)virt_to_phys(share_mem));
+	}
+	else {
+        pid = current->pid;
+        pr_info("current pid %d\n", pid);
+		pid = simple_strtol(buf, 0, 0);
+        if (pid > 0) {
+            tsk = pid_task(find_vpid(pid), PIDTYPE_PID);
+            if (tsk) print_task_info(tsk);
+        }
+	}
+
+	return len;
+}
+
+static DEVICE_ATTR(cmd_buffer, 0660, ext_sync_show, ext_sync_store);
+ 
+static int __init dev_init(void)
+{
+    int ret;
+    unsigned char i;
+    struct device *dev;
+
+    //内存分配
+    share_mem = (unsigned char *)kmalloc(4096, GFP_KERNEL);
+    for(i = 0;i < 128;i++) share_mem[i] = i;
+
+    //将该段内存设置为保留
+    //SetPageReserved(virt_to_page(share_mem));
+
+    //注册混杂设备
+    ret = misc_register(&misc);
+
+    dev = misc.this_device;
+    if (sysfs_create_file(&dev->kobj, &dev_attr_cmd_buffer.attr)) {
+        dev_info(dev, "%s: sysfs create failed", __func__);
+        return -1;
+    }
+
+    return ret;
+}
+ 
+static void __exit dev_exit(void)
+{
+    //注销设备
+    misc_deregister(&misc);
+    //清除保留
+    //ClearPageReserved(virt_to_page(share_mem));
+    //释放内存
+    kfree(share_mem);
+}
+ 
+module_init(dev_init);
+module_exit(dev_exit);
+MODULE_LICENSE("GPL");
+
 
 
 
